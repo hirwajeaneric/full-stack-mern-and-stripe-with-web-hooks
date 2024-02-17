906e2ff5de7c04f33fc919dbaa998c63f0bada88
hirwajeaneric <hirwajeaneric25@gmail.com>
2024-02-17 20:12:49 +0200

    Updates: Final test

--- server_cc/server.js
+++ server_cc/server.js
@@ -8,7 +8,7 @@ const allRoutes = require('./routes');
 const app = express();
 
 const corsOptions = {
-    origin: '*',
+    origin: ['http://localhost:3000', process.env.CLIENT_URL],
     method: 'GET, POST, PUT, DELETE',
     allowedHeaders: 'Contect-Type, Authorization' // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
@@ -17,11 +17,13 @@ app.use(cors(corsOptions));
 app.use(express.json());
 bodyParser.urlencoded({ extended: true });
 
+const port = 4242;
+
 mongoose.connect(process.env.MONGODB_URL)
     .then(() => {
         console.log('MongoDB connected ...');
-        app.listen(4242, () => {
-            console.log('Running on port 4242 ...');
+        app.listen(port, () => {
+            console.log(`Running on port ${port} ...`);
         })
     })
     .catch(err => {
