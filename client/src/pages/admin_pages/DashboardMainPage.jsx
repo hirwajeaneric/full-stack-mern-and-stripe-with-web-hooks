10ad68f9d2d660de1daf6ffbf5abbc5d8d3e0c18
hirwajeaneric <hirwajeaneric25@gmail.com>
2024-02-25 13:54:09 +0200

    Feature: Well designed admin dashboard structure

--- client/src/pages/admin_pages/DashboardMainPage.jsx
+++ client/src/pages/admin_pages/DashboardMainPage.jsx
@@ -1,14 +1,12 @@
-import { Outlet } from "react-router-dom"
-import AdminSideMenuBar from "../../components/AdminSideMenuBar"
+import { Outlet } from "react-router-dom";
+import AdminSideMenuBar from "../../components/AdminSideMenuBar";
 
 const DashboardMainPage = () => {
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center">
-      <div className="flex h-screen flex-wrap w-full max-w-ful justify-between items-start">
-        <AdminSideMenuBar />
-        <div className="w-full md:w-3/4 md:pl-10 mt-10 md:mt-0">
-          <Outlet />
-        </div>
+    <div className="flex md:h-screen flex-wrap w-full max-w-ful justify-start md:justify-between items-start">
+      <AdminSideMenuBar />
+      <div className="md:h-screen w-full md:w-4/5 p-5">
+        <Outlet />
       </div>
     </div>
   )
